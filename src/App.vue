add77bb69c6adea993c7cd8b0cf4ad0e339fcb3b
Enoch <mmejuenoch@gmail.com>
2023-01-13 18:41:59 +0100

    Base files

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  still works
+</template>
+
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "enoch"
+    }
+  }
+}
+</script>
+
+
+<style>
+</style>
